780320fd2914e807a76e79ce0dbdb2b9bcb9700b
elladebbie36 <42787594+elladebbie36@users.noreply.github.com>
2018-09-12 23:30:11 +0100

    Add files via upload

---
+++ a4_10657322.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int Gcd;
+void main ()
+{
+	int a,b, result;
+	printf (Enter the 2 numbers to find the Gcd:);
+	scanf(%d %d ; 8a, 2b);
+	result= gcd (a,b)
+	printf(the Gcd of % and % is %d/n,a,b,result);
+	while (a!,b)
+	{
+		if (a>b)
+		{
+			return Gcd (a^-b,b);
+		}
+		else
+		{
+			return Gcd (a,b,-a);
+		}
+	}
+	return a;
+}
